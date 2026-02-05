0cdef3c05d92a864993107264b5d638fb5714ab8
Elias Ferreira Junior <elias.junior@etipi.pi.gov.br>
2026-02-05 13:28:43 -0300

    Enhance Movies page with Bootstrap components for improved layout and interactivity

--- app/javascript/react/pages/Movies.tsx
+++ app/javascript/react/pages/Movies.tsx
@@ -1,3 +1,6 @@
+import Badge from "react-bootstrap/esm/Badge";
+import Button from "react-bootstrap/esm/Button";
+import Image from "react-bootstrap/Image";
 import { Movie } from "../models";
 import { useMovies } from "../queries/movie";
 
@@ -13,59 +16,76 @@ export default function MoviesPage() {
       {movies && movies.length ? (
         <div className="row g-3">
           {movies.map((movie: Movie) => (
-            <div key={movie.id} className="col-12 col-md-6 col-lg-3">
-              <div className="card shadow-sm h-100">
-                <img
-                  src={movie.cartaz}
-                  className="card-img-top"
-                  alt={movie.titulo}
-                />
+            <div key={movie.id} className="col-6 col-lg-3">
+              <div className="card shadow-sm h-100 border">
+                <div
+                  className="d-flex overflow-auto"
+                  style={{ maxHeight: "400px" }}
+                >
+                  <Image
+                    src={movie.cartaz}
+                    className="card-img-top object-fit-cover"
+                    alt={movie.titulo}
+                    fluid
+                  />
+                </div>
                 <div className="card-header">
-                  <h5 className="card-title mb-0">{movie.titulo}</h5>
+                  <h5 className="card-title mb-0 text-center">
+                    {movie.titulo}
+                  </h5>
                 </div>
-                <div className="bg-success rounded-0 text-wrap text-center">
-                  <strong className="badge">
-                    {movie.semana ? `(${movie.semana})` : null}
-                  </strong>
-                </div>
-                <div className="card-body text-lg-center bg-dark pb-0">
+
+                <Badge
+                  bg="success"
+                  className="rounded-0 text-wrap text-center py-2 shadow-sm"
+                >
+                  {movie.semana ? `(${movie.semana})` : null}
+                </Badge>
+
+                <div className="card-body text-lg-center bg-light">
                   <div className="d-grid gap-2">
-                    <a
-                      className="btn btn-light border btn-sm"
-                      data-bs-toggle="modal"
-                      data-bs-target={`#movie_showtimes_${movie.id}`}
-                      href="#"
+                    <Button
+                      type="button"
+                      variant="light"
+                      className="border shadow-sm"
+                      size="sm"
                     >
                       <i className="bi bi-film me-2"></i>
                       Sessões
-                    </a>{" "}
-                    <a
-                      className="btn btn-light border btn-sm"
-                      title={movie.titulo}
-                      data-fancybox="gallery"
-                      data-caption={movie.titulo}
-                      href={`//www.youtube.com/embed/${movie.trailer}`}
+                    </Button>
+                    <Button
+                      type="button"
+                      variant="light"
+                      className="border shadow-sm"
+                      size="sm"
                     >
                       <i className="bi bi-play-circle me-2"></i>
-                      Trailer
-                    </a>{" "}
-                    <a
-                      className="btn btn-info text-white btn-sm"
-                      data-bs-toggle="modal"
-                      data-bs-target={`#movie_synopsis_${movie.id}`}
-                      href="#"
+                      Trailer{" "}
+                    </Button>
+                    <Button
+                      type="button"
+                      variant="info"
+                      className="border shadow-sm"
+                      size="sm"
                     >
                       <i className="bi bi-file-text me-2"></i>
                       Sinopse
-                    </a>{" "}
-                    <a
-                      className="btn btn-success text-white btn-sm"
-                      target="_blank"
-                      href="https://www.veloxtickets.com/portal/Local/Cinema/Teresina/Cinemas-Teresina/TRS"
+                    </Button>
+                    <Button
+                      type="button"
+                      variant="success"
+                      className="border shadow-sm"
+                      size="sm"
+                      onClick={() =>
+                        window.open(
+                          "https://www.veloxtickets.com/portal/Local/Cinema/Teresina/Cinemas-Teresina/TRS",
+                          "_blank",
+                        )
+                      }
                     >
                       <i className="bi bi-tags me-2"></i>
                       Comprar
-                    </a>{" "}
+                    </Button>
                   </div>
                 </div>
               </div>
